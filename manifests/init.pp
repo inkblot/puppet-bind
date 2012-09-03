08dbd5d4ed76d81de311bd1b9aa99b6145a1d686
Nate Riffe <inkblot@movealong.org>
2012-09-03 14:22:25 -0500

    Define boilerplate acl, view, and zone configs

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,4 +13,32 @@ class bind (
 		hasstatus  => true,
 		require    => Package[$bind::params::bind_package],
 	}
+
+	concat { [
+		"${bind::params::confdir}/acls.conf"
+		"${bind::params::confdir}/views.conf"
+		"${bind::params::confdir}/zones.conf"
+		]:
+		owner => $bind::params::bind_user,
+		group => $bind::params::bind_group,
+		mode  => '0644',
+	}
+
+	concat::fragment { "named-acls-header":
+		order   => '00',
+		target  => "${bind::params::confdir}/acls.conf",
+		content => "# This file is managed by puppet - changes will be lost\n",
+	}
+
+	concat::fragment { "named-views-header":
+		order   => '00',
+		target  => "${bind::params::confdir}/views.conf",
+		content => "# This file is managed by puppet - changes will be lost\n",
+	}
+
+	concat::fragment { "named-zones-header":
+		order   => '00',
+		target  => "${bind::params::confdir}/zones.conf",
+		content => "# This file is managed by puppet - changes will be lost\n",
+	}
 }
