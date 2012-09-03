e575d3a8b7b1e5fb33ac6017d749e9df7527d034
Nate Riffe <inkblot@movealong.org>
2012-09-03 14:34:13 -0500

    Consistency is awesome

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,7 +4,7 @@ class bind::params {
 		'Debian': {
 			$bind_package = 'bind9'
 			$bind_service = 'bind9'
-			$bind_confdir = '/etc/bind'
+			$confdir      = '/etc/bind'
 			$bind_user    = 'bind'
 			$bind_group   = 'bind'
 		}
