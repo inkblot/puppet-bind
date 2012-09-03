a6bbb4c2cda952024e79a23b6a880164678649c4
Nate Riffe <inkblot@movealong.org>
2012-09-03 14:28:30 -0500

    Add missing platform parameters

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,6 +4,9 @@ class bind::params {
 		'Debian': {
 			$bind_package = 'bind9'
 			$bind_service = 'bind9'
+			$bind_confdir = '/etc/bind'
+			$bind_user    = 'bind'
+			$bind_group   = 'bind'
 		}
 		default: {
 			fail("Operating system is not supported ${::osfamily}")
