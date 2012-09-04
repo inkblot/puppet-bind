2d1d6d826da81497e470532b8b57fd9052bfd28e
Nate Riffe <inkblot@movealong.org>
2012-09-03 23:05:12 -0500

    Get debian's default zones accounted for

--- manifests/params.pp
+++ manifests/params.pp
@@ -7,6 +7,22 @@ class bind::params {
 			$confdir      = '/etc/bind'
 			$bind_user    = 'bind'
 			$bind_group   = 'bind'
+
+			file { [
+				"${confdir}/bind.keys",
+				"${confdir}/db.empty",
+				"${confdir}/db.local",
+				"${confdir}/db.root",
+				"${confdir}/db.0",
+				"${confdir}/db.127",
+				"${confdir}/db.255",
+				"${confdir}/named.conf.default-zones",
+				"${confdir}/rndc.key",
+				"${confdir}/zones.rfc1918",
+				]:
+				ensure  => present,
+				require => Package[$bind_package],
+			}
 		}
 		default: {
 			fail("Operating system is not supported ${::osfamily}")
