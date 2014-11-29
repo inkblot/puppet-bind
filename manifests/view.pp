e365f5e49a7f8215bcefbcf1b8d223225f2c48ff
Nate Riffe <inkblot@movealong.org>
2014-11-28 19:02:23 -0600

    Distinguish view access from recursion access
    Make recursion_match_clients and recursion_match_destinations mirroring the
    existing match_clients and match_destinations in order to distinguish recursion
    access as a proper subset of view access.

--- manifests/view.pp
+++ manifests/view.pp
@@ -1,10 +1,12 @@
 # ex: syntax=puppet si ts=4 sw=4 et
 
 define bind::view (
-    $match_clients      = 'any',
-    $match_destinations = '',
-    $zones              = [],
-    $recursion          = true,
+    $match_clients                = 'any',
+    $match_destinations           = '',
+    $zones                        = [],
+    $recursion                    = true,
+    $recursion_match_clients      = 'any',
+    $recursion_match_destinations = '',
 ) {
     $confdir = $bind::params::confdir
 
