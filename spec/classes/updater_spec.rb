ee0e095ea2d8d3aa91381f6643d1dcbf66fcdbeb
Cedric DEFORTIS <cedric.defortis@cassio.pe>
2017-04-27 15:30:59 +0200

    Add facts to updater spec to make it green

--- spec/classes/updater_spec.rb
+++ spec/classes/updater_spec.rb
@@ -1,6 +1,16 @@
 require 'spec_helper'
 
 describe 'bind::updater' do
+  let :facts do
+    {
+      :concat_basedir  => '/wtf',
+      :osfamily        => 'Debian',
+      :os => {
+        :family => 'Debian',
+      },
+      :operatingsystem => 'Debian'
+    }
+  end
   it { should compile }
   it { should compile.with_all_deps }
 end
