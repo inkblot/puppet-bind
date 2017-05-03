13b1137399d818b5656c14c10a8958a7a4a201a7
Cédric Defortis <cedric.defortis@lyra-network.com>
2017-05-03 14:06:33 +0200

    Feature: add $extra_options parameter
    This parameter allow any option to be added to the named.conf options
    {}; directive.

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,6 +10,7 @@ class bind (
     $auth_nxdomain         = undef,
     $include_default_zones = true,
     $include_local         = false,
+    $extra_options         = {},
 ) inherits bind::defaults {
 
     File {
